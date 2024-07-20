b268036a9639efdcef6337f82f80d63015b7c15e
Ibambe Kamanzi Chris <chrisibambe@gmail.com>
2024-07-21 00:06:46 +0300

    Login Screen

---
+++ app/src/main/java/com/chris/to_do_list/Login_Screen.kt
@@ -0,0 +1,11 @@
+package com.chris.to_do_list
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Login_Screen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_screen)
+    }
+}
